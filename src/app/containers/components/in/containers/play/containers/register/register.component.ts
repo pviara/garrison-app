6a61c57f4d61f141fab79ff479036bc20e9b4f21
Pierre Viara <pierre.viara@outlook.com>
2021-10-13 16:11:31 +0200

    feat(register): add filter system

--- src/app/containers/components/in/containers/play/containers/register/register.component.ts
+++ src/app/containers/components/in/containers/play/containers/register/register.component.ts
@@ -12,6 +12,8 @@ import { IUnit } from 'src/models/static/IUnit';
 import { RegisterService } from '../../../../services/dynamic/register.service';
 import { SoundService } from 'src/app/shared/services/sound.service';
 
+type FilterType = 'building' | 'research' | 'unit';
+
 @Component({
     selector: 'garrison-in-play-register',
     templateUrl: './register.component.html',
@@ -21,6 +23,8 @@ export class RegisterComponent implements AfterViewInit, OnInit {
   buildings!: IBuilding[];
   
   character!: ICharacter;
+
+  filteredRecords!: IRecord[];
   
   garrisonId!: string;
   
@@ -29,6 +33,8 @@ export class RegisterComponent implements AfterViewInit, OnInit {
   researches!: IResearch[];
 
   units!: IUnit[];
+
+  private _appliedFilters: FilterType[] = [];
   
   constructor(
     private _route: ActivatedRoute,
@@ -45,6 +51,7 @@ export class RegisterComponent implements AfterViewInit, OnInit {
     this.character = this._route.snapshot.data.character;
     this.garrisonId = this._route.snapshot.data.garrisonId;
     this.records = this._route.snapshot.data.records;
+    this.filteredRecords = this.records;
     this.researches = this._route.snapshot.data.researches;
     this.units = this._route.snapshot.data.units;
 
@@ -54,5 +61,51 @@ export class RegisterComponent implements AfterViewInit, OnInit {
       .subscribe(records => {
         this.records = records || [];
       });
+  }
+
+  areFiltersAllRemoved() {
+    return this._appliedFilters.length === 0;
+  }
+
+  hasFilterBeenApplied(filter: FilterType) {
+    return this
+      ._appliedFilters
+      .find(
+        applied => applied === filter
+      );
+  }
+
+  removeAllFilters() {
+    this._appliedFilters = [];
+    this.filteredRecords = this.records;
+  }
+
+  toggleFilter(filter: FilterType) {
+    const existing = this.hasFilterBeenApplied(filter);
+    if (!existing) {
+      this._appliedFilters.push(filter);
+      this.filteredRecords = this._applyFilter(this._appliedFilters);
+      return;
+    }
+
+    const index = this
+      ._appliedFilters
+      .findIndex(
+        applied => applied === filter
+      );
+    this._appliedFilters.splice(index, 1);
+    this.filteredRecords = this._applyFilter(this._appliedFilters);
+
+    if (this.areFiltersAllRemoved()) {
+      this.filteredRecords = this.records;
+    }
+  }
+
+  private _applyFilter(filters: FilterType[]) {
+    const records = this.records;
+    return records
+      .filter(
+        record => filters.find(filter => record.entity === filter)
+      );
   }
 }
